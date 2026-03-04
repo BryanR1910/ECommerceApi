1954849d255fa3899a231f09c3bfe0b6ee7a432d
= <bryanrodriguezsanchez0@gmail.com>
2026-03-04 15:21:03 -0600

    Update ResourceNotFoundException messages

--- src/main/java/com/bryan/ECommerceApi/exception/ResourceNotFoundException.java
+++ src/main/java/com/bryan/ECommerceApi/exception/ResourceNotFoundException.java
@@ -1,7 +1,19 @@
 package com.bryan.ECommerceApi.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
+    private String resourceName;
+    private String fieldName;
+    private Object fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with: %s = '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public ResourceNotFoundException(String resourceName) {
+        super(String.format("No %s records found in the system.", resourceName));
+        this.resourceName = resourceName;
     }
 }
