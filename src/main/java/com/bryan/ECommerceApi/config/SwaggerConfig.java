07e7df0583d94cff8e2e67c168f38450d22d4660
Bryan <bryanrodriguezsanchez0@gmail.com>
2026-03-19 14:51:36 -0600

    feat: add Swagger/OpenAPI config

---
+++ src/main/java/com/bryan/ECommerceApi/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.bryan.ECommerceApi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.http.HttpHeaders;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "E-Commerce API",
+                description = "E-Commerce platform with cart and payment gateway integration.",
+                version = "1.0.0"
+        ),
+        security = @SecurityRequirement(
+                name = "Security Token"
+        )
+)
+@SecurityScheme(
+        name = "Security Token",
+        description = "Access Token for my API",
+        type = SecuritySchemeType.HTTP,
+        paramName = HttpHeaders.AUTHORIZATION,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {}
